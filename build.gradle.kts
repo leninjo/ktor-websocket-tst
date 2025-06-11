24d3dd619fb47f02e369d8b087b9835531e4cd21
Lenin Lucas <llucas@homeland.com.gt>
2025-06-11 09:21:55 -0600

    add main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,12 @@ repositories {
     mavenCentral()
 }
 
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "io.ktor.server.netty.EngineMain"
+    }
+}
+
 dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.websockets)
